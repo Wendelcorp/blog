5bb05ebdfcdcd7df5315b4448c0f23c09c3411d9
wendelcorp <bryce@brycewendelaar.com>
2017-04-17 18:46:12 -0400

    Updated post styling and added image uploading with paperclip

--- trendy/app/models/post.rb
+++ trendy/app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_attached_file :image, styles: { large: "500x500>", medium: "300x300>", thumb: "150x150>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :title, presence: true, length: { minimum: 4 }
   validates :body, presence: true
 end
